1e675dea6afb2d7d88e33110948dd789fd6c2d8d
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-05-04 12:34:11 -0400

    feat(wip): controlling objs

--- app/app/pages/index.vue
+++ app/app/pages/index.vue
@@ -35,15 +35,30 @@ export default defineComponent({
         },
       });
 
-      // 3. Create bodies
+      // 3. Create bodies, filler assets rn, will replace later
       const boxA = Bodies.rectangle(400, 200, 80, 80);
       const ballA = Bodies.circle(380, 100, 40, { restitution: 0.5 });
       const ground = Bodies.rectangle(400, 580, 810, 40, { isStatic: true });
 
       // 4. Add to world
       World.add(engine.world, [boxA, ballA, ground]);
+      // 5. Event listener
 
-      // 5. Run
+      type keys = {
+        keytype: Boolean
+      } //def broken
+      const keys = {};
+      Matter.Events.on(engine, 'beforeUpdate', function() {
+          const speed = 5;
+          let vx = 0;
+          let vy = 0;
+
+          if (keys['ArrowLeft']) vx = -speed;
+          if (keys['ArrowRight']) vx = speed;
+          // Use setVelocity for crisp movement
+          Matter.Body.setVelocity(ballA, { x: vx, y: vy });
+      });
+      // 6. Run
       Render.run(render);
       runner = Runner.create();
       Runner.run(runner, engine);
