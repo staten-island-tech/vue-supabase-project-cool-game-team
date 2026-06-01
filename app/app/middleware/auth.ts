31ed65b1cafaa251b053ec4481faccd82f72e045
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-06-01 01:35:33 +0000

    fix: redirect to /login if unauth

--- app/app/middleware/auth.ts
+++ app/app/middleware/auth.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useSupabaseUser()
+    if (to.path.startsWith('/auth')) return
     if(!user.value) return navigateTo('/auth/login')
 })
