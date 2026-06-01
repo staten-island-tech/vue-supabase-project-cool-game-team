a6fa0a5aad38ecd72ee4ed94fb1edb055df3e696
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-06-01 15:56:06 +0000

    wip: match search system

--- app/app/pages/search.vue
+++ app/app/pages/search.vue
@@ -1,7 +1,15 @@
 <script setup>
-definePageMeta({middleware: 'auth'})
+//definePageMeta({middleware: 'auth'})
+const supabase = useSupabaseClient()
+async function getMatches(){
+const { data, error } = await supabase.from('matches').select('*')
+if(!error) return data
+}
+async function createMatch(){
+    
+}
 </script>
 
 <template>
-    
+
 </template>
