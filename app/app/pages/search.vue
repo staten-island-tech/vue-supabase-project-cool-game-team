4588d9540c97934af081eb35d98b6799274786c5
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-06-02 12:29:33 -0400

    wip: match sys

--- app/app/pages/search.vue
+++ app/app/pages/search.vue
@@ -2,20 +2,31 @@
 //definePageMeta({middleware: 'auth'})
 const supabase = useSupabaseClient()
 const { data, error } = await supabase.from('matches').select('*')
+const { user, e } = await supabase.auth.getUser()
 
 async function createMatch(){
-    
+    const { data , error } = await supabase.from('matches').insert({players: {p1: user.uuid}})
+}
+
+async function joinMatch(uuid) {
+    const { data , error } = await supabase.from('matches').select('players').eq('uuid', uuid);
+    if(data[0].length > 1) {
+        return 'error'
+    } else {
+        // update to add user to match list
+    } 
 }
 </script>
 
 <template>
 <div class="w-300 h-200 overflow-y-scroll overflow-x-hidden">
     <div v-for="match in data">
-        <div v-if="match.players.length < 2">
-            <p>{{ match.uuid}}</p>
+        <div @click='joinMatch(match.uuid)' v-if="match.players.length < 2">
+            <p>{{ match.uuid }}</p>
             <p>{{ match.players.length }}/2 players</p>
             <p>Created at {{ new Date(match.created_at).toLocaleString()}}</p>
         </div>
     </div>
 </div>
+<button @click="createMatch">createMatch</button>
 </template>
