5b897e1d21e3846cd7779a82711f12af95d6d3f6
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-06-14 22:03:08 -0400

    fix: the actual account redirect fix
    hopefully??? the last fix in the previous commit worked right before i committed it but then didnt work after

--- app/app/middleware/auth.ts
+++ app/app/middleware/auth.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware(async () => {
-    const user = useSupabaseUser()
-    if (!user.value) {
-        return navigateTo('/login')
-    }
+  const user = useSupabaseUser()
+  
+  if (!user.value) {
+    const { data } = await useSupabaseClient().auth.getSession()
+    if (!data.session) return navigateTo('/login')
+  }
 })
